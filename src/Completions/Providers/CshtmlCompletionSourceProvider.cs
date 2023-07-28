efc907429fc32258cb447439b2e13359175c8fa8
Peter Matthews <p_matthews@live.co.uk>
2023-07-28 14:37:09 +0100

    feat: Support for `.cshtml` files.

---
+++ src/Completions/Providers/CshtmlCompletionSourceProvider.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Utilities;
+using System.ComponentModel.Composition;
+using TailwindCSSIntellisense.Completions.Sources;
+using TailwindCSSIntellisense.Settings;
+
+namespace TailwindCSSIntellisense.Completions.Providers
+{
+    /// <summary>
+    /// A factory for creating <see cref="HtmlCompletionSource"/>; exported via MEF.
+    /// </summary>
+    [Export(typeof(ICompletionSourceProvider))]
+    [ContentType("cshtml")]
+    [Name("TailwindCSS CSHTML Token Completion")]
+    internal class CshtmlCompletionSourceProvider : ICompletionSourceProvider
+    {
+        [Import]
+        internal CompletionUtilities CompletionUtils { get; set; }
+        [Import]
+        internal SettingsProvider SettingsProvider { get; set; }
+
+        public ICompletionSource TryCreateCompletionSource(ITextBuffer textBuffer)
+        {
+            return new CshtmlCompletionSource(CompletionUtils, SettingsProvider, textBuffer);
+        }
+    }
+}
